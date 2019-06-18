ac301087a8fad983fed146068027335d80f66269
Johan Desmyter <johan@etic.yt>
2019-06-18 14:52:32 +0300

    add missing license header

--- KalamuDynamiqueConfigBundle.php
+++ KalamuDynamiqueConfigBundle.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the kalamu/dynamique-config-bundle package.
+ *
+ * (c) ETIC Services
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Kalamu\DynamiqueConfigBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
